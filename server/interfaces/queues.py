518c29864f5fc4de5a0d39c4570b7edc55a41e0f
SzymonSt <szymonst2808@gmail.com>
2024-11-09 19:10:50 +0100

    refactor

--- server/interfaces/queues.py
+++
@@ -1,4 +0,0 @@
-import queue
-
-shared_text_queue = queue.Queue()
-shared_speech_queue = queue.Queue()
--- server/socket_server/server.py
+++ server/socket_server/server.py
@@ -1,29 +1,25 @@
 import socket
-from interfaces.queues import shared_speech_queue, shared_text_queue
 
 SOCKET_PORT = 8080
 BUFFER_SIZE = 1024
 
 def socket_server_thread_handler():
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    s.bind('./net/socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', SOCKET_PORT))
     s.listen()
 
     while True:
 
         conn,_ = s.accept()
-        data = None
+        data = ""
         while conn:
-            data = conn.recv(BUFFER_SIZE)
+            data += conn.recv(BUFFER_SIZE)
             if data == b'':
                 break
-        
-            if data: 
-                shared_text_queue.put(data.decode())
-            print(shared_speech_queue.empty())
-            if not shared_speech_queue.empty():
-                data = shared_speech_queue.get()
-                print(f"Data after processing: {data}\n")
-                conn.sendall(data.encode())
+
+        print(data)
+
+            
+            
 
     
